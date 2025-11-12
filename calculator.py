22af280757c6134eb44eb1f7393712ecb16ee0a7
amxenjoyer <dimitri.svetahor@gmail.com>
2025-11-12 09:52:59 -0500

    modified calculator

--- calculator.py
+++ calculator.py
@@ -7,9 +7,18 @@ One function per operation, in order.
 
 # https://github.com/amxenjoyer/lab11.git
 # Partner 1: Dimitri Svetahor
-# Partner 2:
+# Partner 2: Daniel
 
 import math
+
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    else:
+        return math.sqrt(a)
+
+def hypotenuse(a, b):
+    math.hypot(a, b)
 
 # First example
 def add(a, b): 
