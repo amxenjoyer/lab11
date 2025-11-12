cf84c7988866b6542801f97e655a97d380189581
amxenjoyer <dimitri.svetahor@gmail.com>
2025-11-12 08:25:52 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,32 @@ calculator.py
 
 One function per operation, in order.
 """
+
+# https://github.com/amxenjoyer/lab11.git
+# Partner 1: Dimitri Svetahor
+# Partner 2:
+
+import math
+
 # First example
 def add(a, b): 
-    pass
+    return a+b
+
+def sub(a, b):
+    return a-b
+def mul(a, b):
+    return a*b
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    else:
+        return b/a
+def log(a, b):
+    if a < 1 or b < 1:
+        raise ValueError
+    else:
+        return math.log(b, a)
+def exp(a, b):
+    return a**b
 
 
